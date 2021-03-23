74f58b5b65a6e8080d0763bdc707b74252d880b0
108061233 <aa0978505115@gmail.com>
2021-03-23 06:07:38 -0700

    add main.cpp

---
+++ demo_1/main.cpp
@@ -0,0 +1,17 @@
+#include "mbed.h"
+using namespace std::chrono;
+
+Ticker flipper;
+DigitalOut led(LED3);
+
+void flip()
+{
+    led = !led;
+}
+
+int main()
+{
+    led = 0;
+    flipper.attach(&flip, 200ms);
+    while(1){}
+}
